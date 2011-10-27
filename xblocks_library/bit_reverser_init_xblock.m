1a5084424948271dcb522db7742f9d03d7ab1356
Suraj Gowda <sgowda@eecs.berkeley.edu>
2011-10-27 02:03:46 -0700

    modified reorder block to detect a bit reversed pattern (incrementing only for now) and swap in a bit reverser TODO: same thing for the decrementing bit reverse pattern, reversal (the other two reorder patterns from bi_real_unscr_4x)

---
+++ xblocks_library/bit_reverser_init_xblock.m
@@ -0,0 +1,25 @@
+function bit_reverser_init_xblock(blk, varargin)
+
+defaults = { ...
+    'n_bits', 1, ...
+    };
+
+n_bits = get_var('n_bits', 'defaults', defaults, varargin{:});
+
+din = xInport('din');
+dout = xOutport('dout');
+
+slice_outs = {};
+
+for k=1:n_bits
+    slice_out = xSignal;
+    xBlock( struct('source', 'Slice', 'name', ['slice_', num2str(k)]), ...
+        struct('mode', 'Lower Bit Location + Width', 'bit0', k-1, 'base0', 'LSB of Input'), ...
+        {din}, {slice_out});
+    slice_outs{k} = slice_out;
+end
+
+xBlock(struct('source', 'Concat', 'name', 'rev_concat'), ...
+    struct('num_inputs', n_bits), slice_outs, {dout});
+
+end
