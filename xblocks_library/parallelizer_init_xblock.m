be51e63e349234afe23103925207feb4567b2fa9
Hong Chen <chenhong@intelr900-1.EECS.Berkeley.EDU>
2012-03-27 12:28:23 -0700

    Add sync port to parallel-CIC-filter-related blocks (counters, accumulators, etc.)

--- xblocks_library/parallelizer_init_xblock.m
+++ xblocks_library/parallelizer_init_xblock.m
@@ -32,10 +32,12 @@ xilinx = get_var('xilinx', 'defaults', defaults, varargin{:});
 
 
 inport = xInport('in');
+sync_in = xInport('sync_in');
 outports = cell(1,n_outputs);
 for i =1:n_outputs
     outports{i} = xOutport(['out',num2str(i)]);
 end
+sync_out = xOutport('sync_out');
 
 
 delay_ins = cell(1, n_outputs);
@@ -45,29 +47,38 @@ for i =1:n_outputs-1
     delay_ins{i+1} = xSignal(['delay_in',num2str(i+1)]);
     delay_blks{i} = xBlock(struct('source', 'Delay', 'name', ['delay_blk', num2str(i)]), ...
                          {'latency', sample_period}, ...
-                         {delay_ins{i}}, ...
-                         {delay_ins{i+1}});
+                         delay_ins(i), ...
+                         delay_ins(i+1));
 end
 
 downsample_blks = cell(1, n_outputs);
-for i =1:n_outputs
-    if xilinx
+downsample_sync_outs = cell(1, n_outputs);
+if xilinx
+    downsample_sync_outs{1} = xSignal('downsample_sync_out');
+    sync_delay_block = xBlock(struct('source', 'Delay', 'name', 'sync_delay_block'), ...
+                                    struct('latency', 1), ...
+                                    {sync_in}, ...
+                                    downsample_sync_outs(1));
+    for i =1:n_outputs
         downsample_blks{i} = xBlock(struct('source', 'xbsBasic_r4/Down Sample', 'name', ['Down_sample',num2str(i)]), ...
                                        struct('sample_ratio',  n_outputs, ...
                                               'sample_phase','Last Value of Frame  (most efficient)', ...
                                               'latency', 1), ...
-                                  {delay_ins{i}}, ...
-                                  {outports{n_outputs-i+1}});
-    else
+                                  delay_ins(i), ...
+                                  outports(n_outputs-i+1));
+    end
+else
+    for i = 1:n_outputs
+        downsample_sync_outs{i} = xSignal(['downsample_sync_out', num2str(i)]);
         downsample_blks{i} = xBlock(struct('source', str2func('downsample_init_xblock'), 'name', ['Down_sample',num2str(i)]), ...
                                        {[blk, '/','Down_sample',num2str(i)], 'dec_rate',n_outputs,'input_clk_rate', sample_period}, ...
-                                  {delay_ins{i}}, ...
-                                  {outports{n_outputs-i+1}});
-    end
+                                  [delay_ins(i),{sync_in}], ...
+                                  [outports(n_outputs-i+1),downsample_sync_outs(i)]);
+    end   
 end
                         
                         
-
+sync_out.bind(downsample_sync_outs{1});
                         
 if ~isempty(blk) && ~strcmp(blk(1),'/')
     fmtstr =sprintf('Sample Period = %d',sample_period);
